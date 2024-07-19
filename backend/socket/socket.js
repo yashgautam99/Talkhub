eec2cf87932c3bd750c35fe1bd6054dcc18bb2be
Yash Gautam <yash99gautam@gmail.com>
2024-07-19 21:09:24 +0530

    change the production url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://talkhub-5k1a.onrender.com/"],
     methods: ["GET", "POST"],
   },
 });
