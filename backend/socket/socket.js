ac1601fb228a2bb18512ba684d18f1d92f7a459f
Yash Gautam <yash99gautam@gmail.com>
2024-07-19 21:19:54 +0530

    fix some error

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://talkhub-5k1a.onrender.com/"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
